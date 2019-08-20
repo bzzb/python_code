a7d346f1bede17b119e5aaec189d1165ccb2b037
bzzb <aw27153@gmail.com>
2019-08-19 21:33:03 -0700

    this is the initial commitment

---
+++ test.py
@@ -0,0 +1,38 @@
+import math
+import os
+import sys
+from os import rename
+
+import requests
+
+from OpenSSL import SSL
+
+print("   ")
+print("   ")
+print("   ")
+print("   ")
+print("==========Shift+Ctrl+P ==> command pallett ==========================")
+print("====================================")
+print("====================================")
+print(SSL._CERTIFICATE_PATH_LOCATIONS)
+
+print(sys.version)
+print(sys.executable)
+
+print("==========use 'Run Python file in Terminal' ==========================")
+print("==== because 'Run Code Runner' is read-only and cannot accept an input =======")
+
+name = input("Your Name?")
+print("Hello, ", name)
+
+
+def greet(who_to_greet):
+    greeting = "Hello, {}".format(who_to_greet)
+    return greeting
+
+
+print(greet("World"))
+print(greet("Corey"))
+
+r = requests.get("https://www.google.com")
+print(r.status_code)
